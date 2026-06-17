550c7a8493f753f46d3a148ebd1dcfa24704c8c6
wisdom-is-chasing-u-and-u-are-faster <prajwalpatil.sitcomp@gmail.com>
2026-06-17 16:18:57 +0530

    feat(ARCH-689): Change Page Color to Orange
    Refs: ARCH-689

--- app.py
+++ app.py
@@ -1,55 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path="/static")
-
-app.secret_key = b"5ce934008d5a"
+app = Flask(__name__)
 
 
-@app.route("/index")
-@app.route("/")
-def home():
-
-    title = "App Home Page"
-    text = "Hi There , You're At Home Page of App !!"
-    return render_template("page.html", title=title, text=text)
-
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-
-    title = "App Home Page"
-    if not name:
-        text = "Hello, World!  It Is Home Page"
-    else:
-        text = f"Hello {name} !!"
-    return render_template("page.html", name=name, title=title, text=text)
-
-
-@app.route('/myIP')
-def myIP():
-
-    ip = request.remote_addr
-    title = "User / Client IP Address Finder"
-    if not ip:
-        text = "Coudnt Get Your IP"
-    else:
-        text = f"Hello Your IP Address is {ip}"
-    return render_template("page.html", ip=ip, title=title, text=text)
-
-
-@app.route('/test')
-def test():
-    return app.send_static_file("test.html")
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-
-    title = "Page Not Found Error"
-    text = "Your Requested Page Not Found !!!"
-    return render_template('page.html', title=title, text=text), 404
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000" , debug=True)
+    app.run(debug=True)
